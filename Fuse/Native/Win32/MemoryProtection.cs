44caf8d55dc4f5fbf10bd5c21e309c99d47e8d3b
Markus Hauge <markus.hauge@hotmail.com>
2020-02-14 13:31:48 +0100

    Replace tabs with spaces

--- Fuse/Native/Win32/MemoryProtection.cs
+++ Fuse/Native/Win32/MemoryProtection.cs
@@ -1,12 +1,12 @@
 ﻿namespace Fuse.Native.Win32
 {
-	public enum MemoryProtection
-	{
-		NoAccess = 0x01,
-		ReadOnly = 0x02,
-		ReadWrite = 0x04,
-		Execute = 0x10,
-		ExecuteRead = 0x20,
-		ExecuteReadWrite = 0x40
-	}
+    public enum MemoryProtection
+    {
+        NoAccess = 0x01,
+        ReadOnly = 0x02,
+        ReadWrite = 0x04,
+        Execute = 0x10,
+        ExecuteRead = 0x20,
+        ExecuteReadWrite = 0x40
+    }
 }
