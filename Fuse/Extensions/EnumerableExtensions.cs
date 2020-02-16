5a109672d8208310489bdcb7dc49491292a1f841
Markus Hauge <markus.hauge@hotmail.com>
2020-02-17 00:14:19 +0100

    Add enumerate extension method that return tuples of (index, value)

---
+++ Fuse/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Fuse.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<(int, T)> Enumerate<T>(this IEnumerable<T> enumerable) =>
+            enumerable.Select((value, index) => (index, value));
+    }
+}
