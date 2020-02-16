1ad89b3a587aed0a7855a828d4306bf747a2e783
Markus Hauge <markus.hauge@hotmail.com>
2020-02-16 22:49:00 +0100

    Refactor pattern scanner

---
+++ Fuse/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Fuse.Extensions
+{
+    public static class StringExtensions
+    {
+        // Splitting a string into chunks of a certain size
+        // https://stackoverflow.com/a/1450797
+        public static IEnumerable<string> Chunk(this string str, int chunkSize) =>
+            Enumerable
+                .Range(0, str.Length / chunkSize)
+                .Select(i => str.Substring(i * chunkSize, chunkSize));
+    }
+}
--- Fuse/Native/PatternScanner.cs
+++ Fuse/Native/PatternScanner.cs
@@ -1,28 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
+using Fuse.Extensions;
 
 namespace Fuse.Native
 {
     public class PatternScanner
     {
-        public IntPtr Begin { get; }
-        public IntPtr End { get; }
-
-        public PatternScanner(ProcessModule module) : this(module.BaseAddress, module.BaseAddress + module.ModuleMemorySize) { }
-
-        public PatternScanner(IntPtr begin, IntPtr end)
+        private struct ByteMask
         {
-            Begin = begin;
-            End = end;
+            private static ByteMask None => new ByteMask(0x00, 0x00);
+            private static ByteMask Some(byte @byte) => new ByteMask(@byte, 0xFF);
+
+            public static ByteMask From(string part) =>
+                part == "??" ? None : Some(byte.Parse(part, NumberStyles.HexNumber));
+
+            public readonly byte Byte;
+            public readonly byte Mask;
+
+            private ByteMask(byte @byte, byte mask)
+            {
+                Byte = @byte;
+                Mask = mask;
+            }
         }
 
-        private unsafe bool Compare(byte* pBuffer, byte* pBytes, byte* pMask, long length)
+        private static unsafe bool Compare(byte* buffer, IList<ByteMask> byteMasks, long length)
         {
             for (var i = 0; i < length; i++)
             {
-                if ((pBuffer[i] & pMask[i]) != pBytes[i])
+                if ((buffer[i] & byteMasks[i].Mask) != byteMasks[i].Byte)
                 {
                     return false;
                 }
@@ -31,29 +40,30 @@ namespace Fuse.Native
             return true;
         }
 
+        public IntPtr Begin { get; }
+        public IntPtr End { get; }
+
+        public PatternScanner(ProcessModule module) :
+            this(module.BaseAddress, module.BaseAddress + module.ModuleMemorySize) { }
+
+        public PatternScanner(IntPtr begin, IntPtr end)
+        {
+            Begin = begin;
+            End = end;
+        }
+
         public unsafe IntPtr Scan(string pattern)
         {
-            var parts = pattern.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-            var bytes = parts
-                .Select(part => part == "??" ? (byte)0x00 : byte.Parse(part, NumberStyles.HexNumber))
-                .ToArray();
-
-            var mask = parts
-                .Select(part => part == "??" ? (byte)0x00 : (byte)0xFF)
-                .ToArray();
-
+            var parts = pattern.Replace(" ", "").Chunk(2);
+            var byteMasks = parts.Select(ByteMask.From).ToArray();
             var begin = (byte*)Begin.ToPointer();
             var end = (byte*)End.ToPointer();
 
-            fixed (byte* pBytes = bytes, pMask = mask)
+            for (var buffer = begin; buffer < end; buffer++)
             {
-                for (var pBuffer = begin; pBuffer < end; pBuffer++)
+                if (Compare(buffer, byteMasks, byteMasks.Length))
                 {
-                    if (Compare(pBuffer, pBytes, pMask, bytes.Length))
-                    {
-                        return new IntPtr(pBuffer);
-                    }
+                    return new IntPtr(buffer);
                 }
             }
 
