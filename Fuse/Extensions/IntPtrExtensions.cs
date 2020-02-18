b3865fec722520829b030b698c5088ba7506d09d
Markus Hauge <markus.hauge@hotmail.com>
2020-02-18 23:47:58 +0100

    Remove IntPtrExtensions.Read* offset argument

--- Fuse/Extensions/IntPtrExtensions.cs
+++ Fuse/Extensions/IntPtrExtensions.cs
@@ -13,31 +13,31 @@ namespace Fuse.Extensions
         public static IntPtr Offset(this IntPtr pointer, int offset) =>
             pointer + offset;
 
-        public static unsafe byte ReadUInt8(this IntPtr pointer, int offset = 0) =>
-            *(byte*)(pointer + offset).ToPointer();
+        public static unsafe byte ReadUInt8(this IntPtr pointer) =>
+            *(byte*)pointer;
 
-        public static unsafe sbyte ReadInt8(this IntPtr pointer, int offset = 0) =>
-            *(sbyte*)(pointer + offset).ToPointer();
+        public static unsafe sbyte ReadInt8(this IntPtr pointer) =>
+            *(sbyte*)pointer;
 
-        public static unsafe ushort ReadUInt16(this IntPtr pointer, int offset = 0) =>
-            *(ushort*)(pointer + offset).ToPointer();
+        public static unsafe ushort ReadUInt16(this IntPtr pointer) =>
+            *(ushort*)pointer;
 
-        public static unsafe short ReadInt16(this IntPtr pointer, int offset = 0) =>
-            *(short*)(pointer + offset).ToPointer();
+        public static unsafe short ReadInt16(this IntPtr pointer) =>
+            *(short*)pointer;
 
-        public static unsafe uint ReadUInt32(this IntPtr pointer, int offset = 0) =>
-            *(uint*)(pointer + offset).ToPointer();
+        public static unsafe uint ReadUInt32(this IntPtr pointer) =>
+            *(uint*)pointer;
 
-        public static unsafe int ReadInt32(this IntPtr pointer, int offset = 0) =>
-            *(int*)(pointer + offset).ToPointer();
+        public static unsafe int ReadInt32(this IntPtr pointer) =>
+            *(int*)pointer;
 
-        public static unsafe ulong ReadUInt64(this IntPtr pointer, int offset = 0) =>
-            *(ulong*)(pointer + offset).ToPointer();
+        public static unsafe ulong ReadUInt64(this IntPtr pointer) =>
+            *(ulong*)pointer;
 
-        public static unsafe long ReadInt64(this IntPtr pointer, int offset = 0) =>
-            *(long*)(pointer + offset).ToPointer();
+        public static unsafe long ReadInt64(this IntPtr pointer) =>
+            *(long*)pointer;
 
-        public static unsafe IntPtr ReadIntPtr(this IntPtr pointer, int offset = 0) =>
-            *(IntPtr*)(pointer + offset).ToPointer();
+        public static unsafe IntPtr ReadIntPtr(this IntPtr pointer) =>
+            *(IntPtr*)pointer;
     }
 }
