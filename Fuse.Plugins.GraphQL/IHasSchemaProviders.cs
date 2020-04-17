c136347f835da383f003c1cedcc1bf42dc1644fe
Markus Hauge <markus.hauge@hotmail.com>
2020-04-17 19:36:16 +0200

    Remove Fuse.Plugins.GraphQL

--- Fuse.Plugins.GraphQL/IHasSchemaProviders.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Fuse.Plugins.GraphQL
-{
-    public interface IHasSchemaProviders
-    {
-        IEnumerable<ISchemaProvider> SchemaProviders { get; }
-    }
-}
